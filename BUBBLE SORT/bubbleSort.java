5b2c998a999476a03de38511d0a2917b05f9dfcb
Vishnu pratap singh <90092966+Dilmaangemore@users.noreply.github.com>
2022-07-06 04:49:47 +0000

    bubble sort

---
+++ BUBBLE SORT/bubbleSort.java
@@ -0,0 +1,22 @@
+public class bubbleSort {
+  void bubbleSort(int arr[]){
+    int n= arr.length;
+    for(int i=0;i<n-1;i++){
+      for(int =0;j<n-i-1;j++){
+        if(arr[j]>arr[j+1]){
+        int temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }
+      }
+    }
+  }
+
+  void printArray(int arr[]){
+    int n= arr.length;
+    for(int i=0; i<n; i++){
+      System.out.println(arr[i]+" ");
+    }
+    System.out.println();
+  }
+}
