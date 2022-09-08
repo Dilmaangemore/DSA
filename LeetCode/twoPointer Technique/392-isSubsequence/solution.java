dda5d1f243ee74458191e70ed3501d7e1710d5b5
Vishnu pratap singh <90092966+Dilmaangemore@users.noreply.github.com>
2022-09-08 04:17:40 +0000

    leetcode 392 isSubsequence

---
+++ LeetCode/twoPointer Technique/392-isSubsequence/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+    if(s.isEmpty()) return true;
+        int p1 =0; 
+        int p2 =0; 
+        while(p1<s.length() && p2<t.length()){
+
+            if(s.charAt(p1) == t.charAt(p2)){
+                p1++;
+                p2++;
+            }else{
+                p2++;
+            }
+            
+        }
+        if(p1==s.length())
+            return true;
+        return false;
+    }
+    }
