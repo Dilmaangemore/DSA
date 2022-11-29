25713380b5b70aacee58f952de8a365a1ab185f4
Vishnu pratap singh <90092966+Dilmaangemore@users.noreply.github.com>
2022-11-29 19:04:29 +0000

    timeConversion

---
+++ HackerRank/Time Convertion/solution.java
@@ -0,0 +1,96 @@
+/*Given a time in , convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+
+Return '12:01:00'.
+
+
+Return '00:01:00'.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in  hour format
+Returns
+
+string: the time in  hour format
+Input Format
+
+A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+Constraints
+
+All input times are valid
+Sample Input
+
+07:05:45PM
+Sample Output
+
+19:05:45*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    // Write your code here
+String [] time = s.split(":");
+
+int hour = Integer.valueOf(time[0]);
+int min = Integer.valueOf(time[1]);
+int sec = Integer.valueOf(time[2].substring(0,2));
+String ampm = time[2].substring(2,4);
+
+if(ampm.equals("AM") && hour ==12){
+    hour = 0;
+}
+if(ampm.equals("PM") && hour<12){
+    hour = hour +12;
+}
+String re = hour+":"+min+":"+sec; 
+      // output : 19:5: 45
+      //Expected : 19:05:45
+return re;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
